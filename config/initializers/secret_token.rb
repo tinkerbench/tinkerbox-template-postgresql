0c3ed12fd42f19787ef98f611e17d576cd2592aa
Jeff Aldrich <jgaldrich@gmail.com>
2012-05-07 10:54:31 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TinkerboxTemplatePostgresql::Application.config.secret_token = 'a6f062a83983be1b5a26487f28e170e068b8ee0e83db041749a7df0bf8a91bf1c04bc5c17126aafc7d4961039b2672732eecea6cb44b3bbb8c11591297bcf6a0'
